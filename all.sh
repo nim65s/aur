e1a5ddbe2cdb078319bcd4f17a0f792d331e447e
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-04-06 01:58:14 +0200

    bof.

--- all.sh
+++ all.sh
@@ -4,8 +4,8 @@ docker build -t pkg .
 
 for d in *
 do
-    echo $d
     [[ ! -d $d ]] && continue
     [[ $d == '.git' || $d == 'logs' ]] && continue
     ./docker.sh $d > logs/$d.log
+    echo $d $?
 done
