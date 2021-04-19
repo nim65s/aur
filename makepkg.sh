54bb34fca932c1f97105c3b0233522eb66a06b1f
Guilhem Saurel <guilhem.saurel@laas.fr>
2021-04-20 00:46:29 +0200

    repo

--- makepkg.sh
+++ makepkg.sh
@@ -1,9 +1,15 @@
-#!/bin/bash
+#!/bin/bash -eux
 
-set -ex
+makepkg --sign
 
-makepkg
-
-namcap *.pkg.tar.xz
+namcap ./*.pkg.tar.zst
 
 makepkg --printsrcinfo > .SRCINFO
+
+sudo pacman -U ./*.pkg.tar.zst
+
+repo-add --verify --sign  ../repo/nim.db.tar.gz ./*.pkg.tar.zst
+
+mv ./*.pkg.tar.zst{,.sig} ../repo
+
+rsync -avP ../repo repo:/srv/chatons/static
