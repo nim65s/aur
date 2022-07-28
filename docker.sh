c00059410625c29a642caf623b0a6df45e9ba2d8
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-07-28 08:49:41 +0200

    clean bash

--- docker.sh
+++ docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-package=${1:-$(basename $(pwd))}
+package=${1:-$(basename "$(pwd)")}
+package=${package%/}
 
-docker run --rm -i --name $package -v /var/cache/pacman/pkg/:/var/cache/pacman/pkg/ -v /etc/pacman.d/:/etc/pacman.d/ \
-    -t pkg paru -Syu --noconfirm $package
+docker run --rm -i --name "$package" -v /var/cache/pacman/pkg/:/var/cache/pacman/pkg/ -v /etc/pacman.d/:/etc/pacman.d/ \
+    -t pkg paru -Syu --noconfirm "$package"
